88312370f856075feb34da696bec7cfa450952d8
raptarior <qazqww@naver.com>
2024-02-21 18:01:55 +0900

    first commit

---
+++ src/main/java/com/example/myfirstproj/MyfirstprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirstproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myfirstproj/MyfirstprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
